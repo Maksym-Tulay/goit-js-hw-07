0ee23a23718b50d8e34fa7921955120ae7649b03
mm_tulay <109350207+Maksym-Tulay@users.noreply.github.com>
2022-12-22 00:41:36 +0200

    task02__in progress
    Rendering of a markup
    adding evt.preventDefault

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const imageGallery = document.querySelector('.gallery');
+function createImageGallery() {
+  const galleryMarkup = galleryItems.reduce((acc, { original, preview, description }) => acc + 
+    `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`,
+    '');
+
+  imageGallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+};
+
+createImageGallery();
+
+imageGallery.addEventListener('click', onGalleryImageClick);
+
+function onGalleryImageClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return
+  };
+
+//   const imageModal = basicLightbox.create(`
+//     <img
+//       src="${evt.target.dataset.source}"
+//       width = "600"
+//       height = "400"
+//       >`,
+//     {
+//       onShow: imageModal => {document.addEventListener('keydown', onEscapeButtonClick)},
+//       onClose: imageModal => {document.removeEventListener('keydown', onEscapeButtonClick)}
+//     }
+//   );
+
+//   imageModal.show();
+
+//   function onEscapeButtonClick(evt) {
+//     if (evt.code !== 'Escape') {
+//       return
+//     }
+//     imageModal.close();
+//   };
+
+};
+
+
